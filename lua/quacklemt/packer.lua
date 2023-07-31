e2127b60431832002addca552480a5ef644704d2
Andreas <andreas.just.petersen@gmail.com>
2023-07-31 20:42:32 +0200

    New config

---
+++ lua/quacklemt/packer.lua
@@ -0,0 +1,18 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.2',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use "EdenEast/nightfox.nvim" -- Packer
+
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+end)
