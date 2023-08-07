30b903094f12d6b698ebcddb7d9e73d5a55e5020
ajp-abc <ajp@abcsoftwork.com>
2023-08-07 16:24:02 +0200

    Colors

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,10 +1,14 @@
 
+local options = {
+    dim_inactive = true,
+}
+
 local palettes = {
     carbonfox = {
         sel0 = "#3a3838",
     },
 }
 
-require("nightfox").setup({ palettes = palettes })
+require("nightfox").setup({ options = options, palettes = palettes })
 
 vim.cmd("colorscheme carbonfox")
