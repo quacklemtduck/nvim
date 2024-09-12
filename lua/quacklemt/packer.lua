2f68d9d531437f3e858bf86c3347af3486b33259
Andreas <andreas.just.petersen@gmail.com>
2024-09-12 18:02:28 +0200

    surround

--- lua/quacklemt/packer.lua
+++ lua/quacklemt/packer.lua
@@ -77,4 +77,13 @@ return require('packer').startup(function(use)
     use 'mfussenegger/nvim-lint'
 
     use 'sbdchd/neoformat'
+    use({
+        "kylechui/nvim-surround",
+        tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    })
 end)
